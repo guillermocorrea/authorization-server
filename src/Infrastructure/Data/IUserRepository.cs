3452b2b809d0af6e6b0bd0f5693950b5164a5093
Luis Correa <ext-luis.correa@here.com>
2020-06-07 13:35:23 -0500

    Initial IdentityServer4

---
+++ src/Infrastructure/Data/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public interface IUserRepository
+    {
+        Task InsertEntity(string role, string id, string fullName);
+    }
+}
