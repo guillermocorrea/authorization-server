8dd7d17243217b2fe918420391657c2926ef096c
Luis Correa <ext-luis.correa@here.com>
2020-06-08 22:25:21 -0500

    Added sql ef configuration

--- src/AuthorizationServer/Startup.cs
+++ src/AuthorizationServer/Startup.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using AuthorizationServer.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,16 +16,33 @@ namespace AuthorizationServer
 {
   public class Startup
   {
+    public IConfiguration Configuration { get; }
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      var migrationAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
       services.AddIdentityServer()
-        .AddInMemoryApiResources(InMemoryConfig.GetApiResources())
-        .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
+        // .AddInMemoryApiResources(InMemoryConfig.GetApiResources())
+        // .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
         .AddTestUsers(InMemoryConfig.GetUsers())
-        .AddInMemoryClients(InMemoryConfig.GetClients())
-        .AddDeveloperSigningCredential(); //not something we want to use in a production environment;
+        // .AddInMemoryClients(InMemoryConfig.GetClients())
+        .AddDeveloperSigningCredential() //not something we want to use in a production environment;
+        .AddConfigurationStore(opt =>
+        {
+          opt.ConfigureDbContext = c => c.UseSqlServer(Configuration.GetConnectionString("sqlConnection"),
+            sql => sql.MigrationsAssembly(migrationAssembly));
+        })
+        .AddOperationalStore(opt =>
+        {
+          opt.ConfigureDbContext = o => o.UseSqlServer(Configuration.GetConnectionString("sqlConnection"),
+            sql => sql.MigrationsAssembly(migrationAssembly));
+        });
 
       services.AddControllersWithViews();
     }
